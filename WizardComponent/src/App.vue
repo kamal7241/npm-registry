bef56e5cea149c6b0c4a923c6ba356375c1ae4a4
Loai Al-Shakhshir <i.alshakhshir@t2.sa>
2019-10-03 07:32:36 +0000

    Merged PR 601: Add Wizard Component
    Add Wizard Component

---
+++ WizardComponent/src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <div id="app">
+    <FormWizard>
+      <TabContent title="Tab one" icon>Tab one</TabContent>
+      <TabContent title="Tab two" icon>Tab two</TabContent>
+      <TabContent title="Tab three" icon>Tab three</TabContent>
+      <TabContent title="Tab four" icon>Tab four</TabContent>
+      <TabContent title="Tab five" icon>Tab five</TabContent>
+      <TabContent title="Tab six" icon>Tab six</TabContent>
+    </FormWizard>
+  </div>
+</template>
+
+<script>
+import FormWizard from "./components/FormWizard";
+import TabContent from "./components/TabContent";
+
+export default {
+  name: "app",
+  components: {
+    FormWizard,
+    TabContent
+  }
+};
+</script>
+
+<style>
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+</style>
