484fc9d53c3035e4b96ad23ba48525f9aabff91d
Mohammed Saber <mohammedsaber43@gmail.com>
2022-05-10 02:00:07 +0300

    init core ui

---
+++ moj_core-ui/dev/serve.vue
@@ -0,0 +1,45 @@
+<template>
+  <div id="app">
+    <AttachmentField 
+      label='Vue3'
+      name='V3'
+      isRequired
+      isMultiple
+      :maxFileSizeInMega='1'
+      :maxFilesSizeInMega='2'
+      :maxDisplayNameLength='5'
+      :maxAttachments='20'
+      :resetErrorOnSelect='false'
+      :activateInternalErrorPreview='false'
+      :localizations='{actionName: "sdfsdf"}'
+      :value='[{name: "dfgdfgdfg"}]'
+      enableFullnameDisplay
+      validateOnSingleFileSize
+      enableFancyPreview
+      exportInitialFieldMeta
+      @select='logVue3'
+      @error="errVue3"
+    />
+  </div>
+</template>
+
+<script>
+import { defineComponent } from 'vue';
+// Uncomment import and local "components" registration if library is not registered globally.
+// import { MojCoreUiSample } from '@/entry.esm';
+
+export default defineComponent({
+  name: 'ServeDev',
+  // components: {
+  //  MojCoreUiSample,
+  // }
+  methods: {
+    logVue2: files => console.log('V2', files),
+    logVue3: files => console.log('V3', files),
+    errVue3: files => console.log('V3Error', files),
+    errVue2: files => console.log('V2Error', files),
+  }
+});
+</script>
+
+
