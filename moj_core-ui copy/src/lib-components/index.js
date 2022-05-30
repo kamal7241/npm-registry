0730fd38c0d52afb996c632b27f1d07eaec230f4
محمد صابر محمد ابو بكر <mabubakr@moj.gov.sa>
2022-05-30 09:28:58 +0300

    cleanup

--- moj_core-ui copy/src/lib-components/index.js
+++
@@ -1,3 +0,0 @@
-/* eslint-disable import/prefer-default-export */
-export { default as AttachmentField } from './attachment-field.vue'
-export { default as ImageCropper } from './image-cropper/index.vue'
